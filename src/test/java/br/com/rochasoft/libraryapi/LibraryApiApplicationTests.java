2f8f12c87d8d0dcedb7257a96ec313430f169997
Anderson Rocha <anderson_rocha79@hotmail.com>
2021-01-11 20:23:00 -0300

    configurando travis e jococo

--- src/test/java/br/com/rochasoft/libraryapi/LibraryApiApplicationTests.java
+++ src/test/java/br/com/rochasoft/libraryapi/LibraryApiApplicationTests.java
@@ -3,6 +3,12 @@ package br.com.rochasoft.libraryapi;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+// para executar todos os testes e analisar a cobertura de código
+// mvnw test
+// https://www.eclemma.org/jacoco/trunk/doc/index.html
+// https://travis-ci.com/
+// https://about.codecov.io/
+// https://github.com/codecov/example-java
 
 
 @SpringBootTest
