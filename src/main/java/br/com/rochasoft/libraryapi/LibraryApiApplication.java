c0b7211336299ee694d81d56f4b0e3ec18f37e58
Anderson Rocha <anderson_rocha79@hotmail.com>
2021-01-11 22:44:40 -0300

    finalização do projeto do curso

--- src/main/java/br/com/rochasoft/libraryapi/LibraryApiApplication.java
+++ src/main/java/br/com/rochasoft/libraryapi/LibraryApiApplication.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -17,9 +18,14 @@ import java.util.List;
 // monitoramento > localhost:8080/actuator
 // arquivo de log > http://localhost:8080/actuator/logfile
 
+// gerando o pacote e executando
+// mvnw clean package
+// java -jar library-api-0.0.2-SNAPSHOT.jar
+// http://localhost:8080/swagger-ui.html
+
 @SpringBootApplication
 @EnableScheduling			// habilita o agendamento de tarefas
-public class LibraryApiApplication
+public class LibraryApiApplication extends SpringBootServletInitializer
 {
 
 	/*
